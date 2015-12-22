15a6bd58e8731462a1ef26f6410d2ed5dec7b2d7
AyushKesar <ayush.kesarwani@gmail.com>
2015-12-22 21:38:06 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,14 @@
+# twitter hashtag to find
+TWITTER_HASHTAG = "#StarWars"
+
+
+# twitter api credentials (https://apps.twitter.com/)
+#TWITTER_CONSUMER_KEY = "your-consumer-key"
+#TWITTER_CONSUMER_SECRET = "your-consumer-secret"
+#TWITTER_ACCESS_TOKEN = "your-access-token"
+#TWITTER_ACCESS_TOKEN_SECRET = "your-access-token-secret"
+
+TWITTER_CONSUMER_KEY = '6NQGaLk3qIjGKSfarUchJWopm'
+TWITTER_CONSUMER_SECRET = '91HTd4wA7VXY9iCKTyyHdTLvXrnpnCYoURMG4dULonwERXa0vM'
+TWITTER_ACCESS_TOKEN = '121034309-dqpCYKGK2hGXHoUdGlTwRMJ4ew8KS0i0WpRHHr42'
+TWITTER_ACCESS_TOKEN_SECRET = 'ttTuGW1e5TsUHdmVHE84fIsbTAwuvSYhJ7HQhl7KXKsqN'
